9fa819e29ccba8c4a0ebd7f093136d1f9d02e263
AdelTamer <adeltammer@gmail.com>
2026-03-06 01:57:34 +0200

    test speed again

--- app/src/main/java/com/fcitu/smartfix/MainActivity.kt
+++ app/src/main/java/com/fcitu/smartfix/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             SmartFixTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "test",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
