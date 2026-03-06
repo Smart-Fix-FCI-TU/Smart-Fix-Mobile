b9edfcbb2155d51d2c0a6ffc1bf06da20d456ff4
AdelTamer <adeltammer@gmail.com>
2026-03-06 02:33:07 +0200

    test update develop branch

--- app/src/main/java/com/fcitu/smartfix/MainActivity.kt
+++ app/src/main/java/com/fcitu/smartfix/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             SmartFixTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Smart Fix || FCI TU",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
