3274c0962707308877cf689797644358dc643dfb
AdelTamer <adeltammer@gmail.com>
2026-03-06 03:30:13 +0200

    undo delete greeting preview

--- app/src/main/java/com/fcitu/smartfix/MainActivity.kt
+++ app/src/main/java/com/fcitu/smartfix/MainActivity.kt
@@ -37,3 +37,11 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SmartFixTheme {
+        Greeting("Android")
+    }
+}
