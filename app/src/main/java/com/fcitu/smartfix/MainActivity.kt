33423fcd8d79329192b6ab6cfa3f20d3476d3933
AdelTamer <adeltammer@gmail.com>
2026-04-29 23:43:04 +0300

    Clean up SmartFixTheme import in MainActivity

--- app/src/main/java/com/fcitu/smartfix/MainActivity.kt
+++ app/src/main/java/com/fcitu/smartfix/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.fcitu.smartfix.ui.designSystem.theme.SmartFixTheme
 import com.fcitu.smartfix.ui.navigation.SmartFixNavGraph
 
 class MainActivity : ComponentActivity() {
@@ -11,7 +12,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            _root_ide_package_.com.fcitu.smartfix.ui.designSystem.theme.SmartFixTheme {
+            SmartFixTheme {
                 SmartFixNavGraph()
             }
         }
