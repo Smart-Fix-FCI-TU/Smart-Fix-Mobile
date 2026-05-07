88bc9506031bc4b36c8dd174cb26309cb569ffea
AdelTamer <adeltammer@gmail.com>
2026-05-07 16:32:57 +0300

    Extract authentication endpoints to NetworkConstants

--- app/src/main/java/com/fcitu/smartfix/data/remote/NetworkConstants.kt
+++ app/src/main/java/com/fcitu/smartfix/data/remote/NetworkConstants.kt
@@ -4,4 +4,10 @@ import com.fcitu.smartfix.BuildConfig
 
 object NetworkConstants {
     const val BASE_URL: String = BuildConfig.BASE_URL
+
+    //  region Authentication endpoints
+    const val LOGIN: String = "api/v1/auth/login"
+    const val REFRESH_TOKEN = "api/v1/auth/refresh-token"
+
+    // endregion
 }
--- app/src/main/java/com/fcitu/smartfix/data/remote/service/AuthService.kt
+++ app/src/main/java/com/fcitu/smartfix/data/remote/service/AuthService.kt
@@ -1,5 +1,7 @@
 package com.fcitu.smartfix.data.remote.service
 
+import com.fcitu.smartfix.data.remote.NetworkConstants.LOGIN
+import com.fcitu.smartfix.data.remote.NetworkConstants.REFRESH_TOKEN
 import com.fcitu.smartfix.data.remote.dto.auth.LoginRequest
 import com.fcitu.smartfix.data.remote.dto.auth.LoginResponse
 import com.fcitu.smartfix.data.remote.dto.auth.TokenRefreshRequest
@@ -9,12 +11,12 @@ import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AuthService {
-    @POST("api/v1/auth/login")
+    @POST(LOGIN)
     suspend fun login(
         @Body request: LoginRequest
     ): LoginResponse
 
-    @POST("api/v1/auth/refresh-token")
+    @POST(REFRESH_TOKEN)
     fun refreshToken(
         @Body request: TokenRefreshRequest
     ): Call<TokenRefreshResponse>
