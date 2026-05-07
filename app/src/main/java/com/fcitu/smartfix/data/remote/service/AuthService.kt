eabaa25994f80a0729ce026a4e1da10f4b64649b
AdelTamer <adeltammer@gmail.com>
2026-05-07 16:28:15 +0300

    Add refreshToken endpoint to AuthService and remove user-type header from login

--- app/src/main/java/com/fcitu/smartfix/data/remote/service/AuthService.kt
+++ app/src/main/java/com/fcitu/smartfix/data/remote/service/AuthService.kt
@@ -2,14 +2,20 @@ package com.fcitu.smartfix.data.remote.service
 
 import com.fcitu.smartfix.data.remote.dto.auth.LoginRequest
 import com.fcitu.smartfix.data.remote.dto.auth.LoginResponse
+import com.fcitu.smartfix.data.remote.dto.auth.TokenRefreshRequest
+import com.fcitu.smartfix.data.remote.dto.auth.TokenRefreshResponse
+import retrofit2.Call
 import retrofit2.http.Body
-import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface AuthService {
     @POST("api/v1/auth/login")
     suspend fun login(
-        @Header("user-type") userType: String,
         @Body request: LoginRequest
     ): LoginResponse
+
+    @POST("api/v1/auth/refresh-token")
+    fun refreshToken(
+        @Body request: TokenRefreshRequest
+    ): Call<TokenRefreshResponse>
 }
