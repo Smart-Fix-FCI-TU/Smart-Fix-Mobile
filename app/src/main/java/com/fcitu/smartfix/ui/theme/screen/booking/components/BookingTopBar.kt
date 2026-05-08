10d0e6d05ff6deab3e391b27e518113197e78f7b
yousef-ELHMISI <114592697+yousef-ELHMISI@users.noreply.github.com>
2026-05-09 02:36:32 +0300

    refactor the name of BookingTopBar and modify the icon

---
+++ app/src/main/java/com/fcitu/smartfix/ui/theme/screen/booking/components/BookingTopBar.kt
@@ -0,0 +1,26 @@
+package com.fcitu.smartfix.ui.theme.screen.booking.components
+
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import com.fcitu.smartfix.R
+import com.fcitu.smartfix.ui.theme.designSystem.components.appBar.AppBar
+
+@Composable
+fun BookingTopBar(
+    onNavigateBack: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    AppBar(
+        title = "Problem Description",
+        leadingContent = {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_arrow_back),
+                contentDescription = "Back"
+            )
+        },
+        onLeadingClick = onNavigateBack,
+        modifier = modifier
+    )
+}
