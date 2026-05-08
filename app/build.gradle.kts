d9a40cf33ec590018c1d75475b9107a88aba00b3
yousef-ELHMISI <114592697+yousef-ELHMISI@users.noreply.github.com>
2026-05-09 02:41:35 +0300

    Remove material-icons-extended dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,7 +57,6 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation("androidx.compose.material:material-icons-extended")
     implementation(libs.compose.navigation)
     implementation(libs.compose.lifecycle.runtime)
 
