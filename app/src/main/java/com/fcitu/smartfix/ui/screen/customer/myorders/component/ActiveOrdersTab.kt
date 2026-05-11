daf43397d8d48e979537b02bd03fdc226329056e
Fouad-Elmeligy <fouadelmeligy30@gmail.com>
2026-05-11 21:03:07 +0300

    -Create( OrdersEmptyState.kt ) -Edit( ActiveOrdersTab.kt + HistoryOrdersTab.kt) -Add( build_icon.png )

--- app/src/main/java/com/fcitu/smartfix/ui/screen/customer/myorders/component/ActiveOrdersTab.kt
+++ app/src/main/java/com/fcitu/smartfix/ui/screen/customer/myorders/component/ActiveOrdersTab.kt
@@ -1,11 +1,63 @@
 package com.fcitu.smartfix.ui.screen.customer.myorders.component
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.fcitu.smartfix.ui.screen.customer.myorders.MyOrdersInteractionListener
 import com.fcitu.smartfix.ui.screen.customer.myorders.MyOrdersUiState
 
 @Composable
-fun ActiveOrdersTab(state: MyOrdersUiState,listener: MyOrdersInteractionListener, modifier: Modifier = Modifier) {
+fun ActiveOrdersTab(
+    state: MyOrdersUiState,
+    listener: MyOrdersInteractionListener,
+) {
+    when {
+        state.isLoadingActive -> {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                CircularProgressIndicator(
+                    color = Color.White,
+                    strokeWidth = 3.dp,
+                    trackColor = Color(0xFFFF4A08)
+                )
+            }
+        }
 
+        !state.hasActiveOrders -> {
+            OrdersEmptyState(
+                "No Active Orders", "All current orders that have not yet been completed \n" +
+                        "will appear here."
+            )
+        }
+
+        state.hasActiveOrders -> {
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                contentPadding = PaddingValues(16.dp),
+                verticalArrangement = Arrangement.spacedBy(12.dp),
+            ) {
+                items(items = state.activeOrders, key = { it.id }) { activeOrder ->
+                    ActiveOrderCard(
+                        order = activeOrder,
+                        technician = listener.onGetTechnicianInfo(activeOrder.technician.id),
+                        onActiveOrderClicked = { listener.onActiveOrderClicked(orderId = activeOrder.id) },
+                        onChatClicked = {}
+                    )
+                }
+            }
+        }
+
+    }
 }
--- app/src/main/java/com/fcitu/smartfix/ui/screen/customer/myorders/component/HistoryOrdersTab.kt
+++ app/src/main/java/com/fcitu/smartfix/ui/screen/customer/myorders/component/HistoryOrdersTab.kt
@@ -1,2 +1,61 @@
 package com.fcitu.smartfix.ui.screen.customer.myorders.component
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.fcitu.smartfix.ui.screen.customer.myorders.MyOrdersInteractionListener
+import com.fcitu.smartfix.ui.screen.customer.myorders.MyOrdersUiState
+
+@Composable
+fun HistoryOrdersTab(
+    state: MyOrdersUiState,
+    listener: MyOrdersInteractionListener,
+) {
+    when {
+        state.isLoadingHistory -> {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                CircularProgressIndicator(
+                    color = Color.White,
+                    strokeWidth = 3.dp,
+                    trackColor = Color(0xFFFF4A08)
+                )
+            }
+        }
+
+        !state.hasHistoryOrders -> {
+            OrdersEmptyState(
+                "No Past Orders", "All successfully completed orders will appear here."
+            )
+        }
+
+        state.hasHistoryOrders -> {
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                contentPadding = PaddingValues(16.dp),
+                verticalArrangement = Arrangement.spacedBy(12.dp),
+            ) {
+                items(items = state.activeOrders, key = { it.id }) { activeOrder ->
+                    CompletedOrderCard(
+                        order = activeOrder,
+                        technician = listener.onGetTechnicianInfo(activeOrder.technician.id),
+                        onCompletedOrderClicked = {},
+                    )
+                }
+            }
+        }
+
+    }
+}
